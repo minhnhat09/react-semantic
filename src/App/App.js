5fb3bf7f2d44b929e9955785beb503b572d33b3b
NGUYEN Nhat Minh <naht-minh.nguyen_externe@groupe-sma.fr>
2019-03-04 16:27:23 +0100

    refactoring

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import { history } from "../_helpers";
 import { PrivateRoute } from "../_components";
 import { HomePage } from "../HomePage";
 import { LoginPage } from "../LoginPage";
+import Header from "../_components/Header";
 class App extends React.Component {
   render() {
     return (
       <Router history={history}>
         <div>
-          <PrivateRoute exact path="/" component={HomePage} />
-          <Route path="/login" component={LoginPage} />
+          <Header />
+          <Switch>
+            <PrivateRoute exact path="/" component={HomePage} />
+            <Route path="/login" component={LoginPage} />
+          </Switch>
         </div>
       </Router>
     );

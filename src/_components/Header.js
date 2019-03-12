b3ce1af4b1f17e8a6b68b1c62ded147e3786328a
NGUYEN Nhat Minh <naht-minh.nguyen_externe@groupe-sma.fr>
2019-03-12 15:32:06 +0100

    toggle header when in login page

--- src/_components/Header.js
+++ src/_components/Header.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-const Header = (props) => {
-  console.log(props);
-  return (
-    <div className="ui secondary pointing menu">
-      <Link to="/" className="item">
-        Home
-      </Link>
-      <Link to="/blogs" className="item">
-        Books
-      </Link>
-      <div className="right menu">
-        <Link to="/login" className="item">
-          Logout
+import { history } from "../_helpers";
+const Header = () => {
+  console.log(history.location.pathname);
+  if (history.location.pathname !== "/login") {
+    return (
+      <div className="ui secondary pointing menu">
+        <Link to="/" className="item">
+          Home
         </Link>
+        <Link to="/blogs" className="item">
+          Blogs
+        </Link>
+        <div className="right menu">
+          <Link to="/login" className="item">
+            Logout
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <div></div>
 };
 
 export default Header;

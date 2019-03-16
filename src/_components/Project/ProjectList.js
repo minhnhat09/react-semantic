44a6c231359d6cee6260cc76f3454720825f3f37
NGUYEN Nhat Minh <nhatminh1809@gmail.com>
2019-03-16 17:15:31 +0100

    create cards

--- src/_components/Project/ProjectList.js
+++ src/_components/Project/ProjectList.js
@@ -2,15 +2,49 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchProjects } from '../../_actions';
+import Matthew from './matthew.png';
 
 class ProjectList extends React.Component {
 	componentDidMount() {
 		this.props.fetchProjects();
 	}
+
+	renderList() {
+		const avatars = [];
+		avatars.push(1);
+		avatars.push(2);
+		avatars.push(3);
+		avatars.push(3);
+		return avatars.map((a, i) => {
+			return (
+				<div key={i} className="card">
+					<div className="image">
+						<img alt="" src={Matthew} />
+					</div>
+					<div className="content">
+						<div className="header">Matt Giampietro</div>
+						<div className="meta">
+							<a>Friends</a>
+						</div>
+						<div className="description">Matthew is an interior designer living in New York.</div>
+					</div>
+					<div className="extra content">
+						<span className="right floated">Joined in 2013</span>
+						<span>
+							<i className="user icon" />
+							75 Friends
+						</span>
+					</div>
+				</div>
+			);
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				ProjectList
+				<div className="ui link cards">{this.renderList()}</div>
 				<div style={{ textAlign: 'right' }}>
 					<Link to="/projects/new" className="ui button primary">
 						Create Project

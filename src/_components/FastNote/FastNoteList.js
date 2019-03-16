8150d6e514b7e997111a0631863633ee69cc0a7f
NGUYEN Nhat Minh <nhatminh1809@gmail.com>
2019-03-16 17:57:27 +0100

    design fast note

--- src/_components/FastNote/FastNoteList.js
+++ src/_components/FastNote/FastNoteList.js
@@ -1,9 +1,37 @@
-import React from "react";
-
+import React from 'react';
+import Matthew from '../../_images/matthew.png';
 class FastNoteList extends React.Component {
-  render() {
-    return <div>FastNoteList</div>;
-  }
+	render() {
+		return (
+			<div>
+				<h3 class="ui header">Third header</h3>
+				<div class="ui feed">
+					<div className="event">
+						<div className="label">
+							<img src={Matthew} />
+						</div>
+						<div className="content">
+							<div className="summary">
+								<a>Joe Henderson</a> posted on his page
+								<div className="date">3 days ago</div>
+							</div>
+							<div>
+								Ours is a life of constant reruns. We're always circling back to where we'd we started,
+								then starting all over again. Even if we don't run extra laps that day, we surely will
+								come back for more of the same another day soon.
+							</div>
+							<div className="meta">
+								<a className="like">
+									<i className="like icon" /> 5 Likes
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			
+			</div>
+		);
+	}
 }
 
 export default FastNoteList;
--- src/_components/Project/ProjectList.js
+++ src/_components/Project/ProjectList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchProjects } from '../../_actions';
-import Matthew from './matthew.png';
+import Matthew from '../../_images/matthew.png';
 
 class ProjectList extends React.Component {
 	componentDidMount() {

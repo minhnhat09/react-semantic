7a84deda61ef56a9806783174eeff7280cf5296b
NGUYEN Nhat Minh <nhatminh1809@gmail.com>
2019-03-17 10:09:32 +0100

    design card flash card

--- src/_components/FastNote/FastNoteList.js
+++ src/_components/FastNote/FastNoteList.js
@@ -4,23 +4,24 @@ class FastNoteList extends React.Component {
 	render() {
 		return (
 			<div>
-				<h3 class="ui header">Third header</h3>
 				<div class="ui feed">
-					<div className="event">
+					<h3 class="ui header">17/03/2019</h3>
+					<div className="event" style={{ background: 'white' }}>
 						<div className="label">
-							<img src={Matthew} />
+							<i class="pencil alternate blue circle icon" />
+							<i class="minus red circle icon" />
 						</div>
 						<div className="content">
 							<div className="summary">
 								Ý tưởng làm app
-								<div className="date">3 days ago</div>
+								<div className="date">09:52</div>
 							</div>
 							<div>
 								Ours is a life of constant reruns. We're always circling back to where we'd we started,
 								then starting all over again. Even if we don't run extra laps that day, we surely will
 								come back for more of the same another day soon.
 							</div>
-							<div className="meta">
+							<div>
 								<i className="ui red tag label">algo</i>
 								<i className="ui yellow tag label">data</i>
 							</div>
--- src/_components/FlashCard/FlashCardList.js
+++ src/_components/FlashCard/FlashCardList.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchFlashCards } from '../../_actions';
 
-
 class FlashCardList extends React.Component {
 	componentDidMount() {
 		this.props.fetchFlashCards();
@@ -12,6 +11,35 @@ class FlashCardList extends React.Component {
 		return (
 			<div>
 				FlashCardList
+				<div class="ui cards">
+					<div class="card">
+						<div class="content">
+							<div class="ui feed">
+								<div className="event">
+									<div className="label">
+										<i class="pencil alternate blue circle icon" />
+										<i class="minus red circle icon" />
+									</div>
+									<div className="content">
+										<div className="summary">
+											Ý tưởng làm app
+											<div className="date">09:52</div>
+										</div>
+										<div>
+											Ours is a life of constant reruns. We're always circling back to where we'd
+											we started, then starting all over again. Even if we don't run extra laps
+											that day, we surely will come back for more of the same another day soon.
+										</div>
+										<div>
+											<i className="ui red tag label">algo</i>
+											<i className="ui yellow tag label">data</i>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 				<div style={{ textAlign: 'right' }}>
 					<Link to="/flash-cards/new" className="ui button primary">
 						Create FlashCard
